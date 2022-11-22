10ac30912fbd0c39577c5827c9783c463290c820
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2022-11-22 10:02:26 -0600

    Add base files

---
+++ battleship_launcher.py
@@ -0,0 +1,10 @@
+'''
+battleship_launcher.py
+Pawelski
+11/22/22
+Launches Battleship program.
+'''
+
+import battleship
+
+b = battleship.Battleship()
---
+++ player.py
@@ -0,0 +1,28 @@
+'''
+player.py
+Pawelski
+11/22/22
+'''
+
+import battleship_ui, board, ship
+
+class Player():
+    '''
+    Implements all the player tasks.
+    '''
+    
+    def __init__(self):
+        '''
+        Creates a new Player object. Initializes both the radar and ocean boards.
+        Then calls the placeShips() method to have the user place all the ships
+        on the ocean board.
+        '''
+        self.rader = board.Board()
+        self.ocean = board.Board()
+        self.placeShips()
+    
+    def place_ships():
+        '''
+        Places all the ships on the ocean board.
+        '''
+    
