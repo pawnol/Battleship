c0971daa246221326f35811ad363f49752ee4314
pawnol <32725534+pawnol@users.noreply.github.com>
2022-11-28 21:04:15 -0600

    Add package infrastructure

--- Tests/test_board.py
+++ Tests/test_board.py
@@ -1,5 +1,5 @@
-from Battleship import player_helpers
-from Battleship import battleship_enumerations as enum
+import Battleship.player_helpers as player_helpers
+import Battleship.battleship_enumerations as enum
 
 b = player_helpers.Board()
 assert b.get_piece(enum.Row.A, 0) == enum.Piece.EMPTY, "Should be EMPTY"
